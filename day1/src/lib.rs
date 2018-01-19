c0c37bb75086fd9a35eae0fc0b9f2470ef3ee50b
Preston Tunnell Wilson <tunpm-16@rhodes.edu>
2018-01-18 22:45:47 -0500

    add part two

--- day1/src/lib.rs
+++ day1/src/lib.rs
@@ -9,6 +9,24 @@ pub fn captcha(i: &[usize]) -> usize {
     sum()
 }
 
+/// split into two,
+/// filter if the same,
+/// then sum up
+pub fn part_two(i: &[usize]) -> usize {
+  let offset = i.len() / 2;
+  let (first, second) = i.split_at(offset);
+  // zip first and second together for filter
+  let result: usize = first.iter().zip(second.iter()).
+    filter(|c| c.0 == c.1).
+    map(|c| c.1).
+    sum();
+  result * 2
+}
+
+fn get_index(start: usize, len: usize, offset: usize) -> usize {
+  (start + offset) % len
+}
+
 pub fn string_to_vec(i: &str) -> Vec<usize> {
   let ret = i.chars().map(|c| c.to_digit(10).unwrap() as usize).
   collect::<Vec<_>>();
@@ -24,6 +42,16 @@ mod test {
     assert_eq!(captcha(&vec![1, 1, 1, 1]), 4);
     assert_eq!(captcha(&vec![1, 2, 3, 4]), 0);
     assert_eq!(captcha(&vec![9, 1, 2, 1, 2, 1, 2, 9]), 9);
+  }
+
+#[test]
+  fn part_two() {
+    use super::part_two;
+    assert_eq!(part_two(&vec![1, 2, 1, 2]), 6);
+    assert_eq!(part_two(&vec![1, 2, 2, 1]), 0);
+    assert_eq!(part_two(&vec![1, 2, 3, 4, 2, 5]), 4);
+    assert_eq!(part_two(&vec![1, 2, 3, 1, 2, 3]), 12);
+    assert_eq!(part_two(&vec![1, 2, 1, 3, 1, 4, 1, 5]), 4);
   }
 
   #[test]

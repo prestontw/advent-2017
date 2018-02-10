3302be7b2090c161ccfd475ec66cccf14061683b
Preston Tunnell Wilson <tunpm-16@rhodes.edu>
2018-02-09 23:35:06 -0500

    add second part

--- day3/src/lib.rs
+++ day3/src/lib.rs
@@ -1,3 +1,121 @@
+// use a hashmap of hashmaps,
+// a function to get indices to look up
+// map over these, sum up
+// function to get next pair of indices based on position
+
+use std::collections::HashMap;
+
+pub fn first_value_bigger(target: usize) -> usize {
+    let mut grid: HashMap<isize, HashMap<isize, usize>> = HashMap::new();
+    let first: HashMap<isize, usize> = [(0, 1)].into_iter().cloned().collect();
+    grid.insert(0, first);
+    let mut coord = (1, 0);
+    loop {
+        let to_insert = sum_around(&grid, coord);
+        if to_insert > target {
+            return to_insert
+        }
+        grid.entry(coord.0).or_insert(HashMap::new())
+            .entry(coord.1).or_insert(to_insert);
+        coord = get_next_index(coord);
+    }
+}
+
+enum Time {
+    Two,
+    Five,
+    Eight,
+    Eleven,
+}
+
+fn get_neighboring_indices(cur: (isize, isize)) -> Vec<(isize, isize)> {
+    let (x, y) = cur;
+    vec![(x - 1, y + 1), (x, y + 1), (x + 1, y + 1),
+         (x - 1, y),                 (x + 1, y),
+         (x - 1, y - 1), (x, y - 1), (x + 1, y - 1)]
+}
+
+fn sum_around(grid: &HashMap<isize, HashMap<isize, usize>>, coord: (isize, isize)) -> usize {
+    let neighbors_indices = get_neighboring_indices(coord);
+    neighbors_indices.into_iter().map(|(x, y)|
+                                      if let Some(ref d) = grid.get(&x) {
+                                          *d.get(&y).unwrap_or(&0)
+                                      }
+                                      else {
+                                          0
+                                      }
+                                      ).sum()
+} 
+
+fn produce_quadrant(x: isize, y: isize) -> Time {
+    use Time::*;
+    if x >= 0 {
+        if y >= 0 {
+            Two
+        }
+        else {
+            Five
+        }
+    }
+    else {
+        if y >= 0 {
+            Eleven
+        }
+        else {
+            Eight
+        }
+    }
+}
+
+fn get_next_index(cur: (isize, isize)) -> (isize, isize) {
+    let (x, y) = cur;
+    if cur == (0, 0) {
+        (1, 0)
+    }
+    else {
+        match produce_quadrant(x, y) {
+            Time::Two => if x > y {
+                (x, y + 1)
+            }
+            else {
+                (x - 1, y)
+            },
+            Time::Eleven => if -x < y {
+                (x - 1, y)
+            }
+            else {
+                (x, y - 1)
+            },
+            Time::Eight => if y - 1 > x {
+                (x, y - 1)
+            }
+            else {
+                (x + 1, y)
+            },
+            Time::Five => if x <= -y {
+                (x + 1, y)
+            }
+            else {
+                (x, y + 1)
+            }
+        }
+    }
+}
+#[test]
+fn test_next_index() {
+    assert_eq!(get_next_index((0, 0)), (1, 0));
+    assert_eq!(get_next_index((1, 0)), (1, 1));
+    assert_eq!(get_next_index((1, 1)), (0, 1));
+    assert_eq!(get_next_index((0, 1)), (-1, 1));
+    assert_eq!(get_next_index((-1, 1)), (-1, 0));
+    assert_eq!(get_next_index((-1, 0)), (-1, -1));
+    assert_eq!(get_next_index((-1, -1)), (0, -1));
+    assert_eq!(get_next_index((0, -1)), (1, -1));
+    assert_eq!(get_next_index((0, -1)), (1, -1));
+    assert_eq!(get_next_index((1, -1)), (2, -1));
+    assert_eq!(get_next_index((2, -1)), (2, 0));
+}
+
 pub fn distance(s: usize) -> usize {
     let (num_square, lower_right_number) = get_nth_square(s);
     let quads = get_quadrants(lower_right_number, num_square);

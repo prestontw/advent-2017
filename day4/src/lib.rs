48f205912ede49433c12a34ffb892e3dc44162eb
Preston Tunnell Wilson <tunpm-16@rhodes.edu>
2018-02-11 23:45:43 -0500

    fight with rust val's not living long enough
    and just make a function over Strings

--- day4/src/lib.rs
+++ day4/src/lib.rs
@@ -4,18 +4,43 @@ fn str_to_passphrase(i: &str) -> Vec<&str> {
     i.split_whitespace().collect()
 }
 
-fn str_to_passphrases(i: &str) -> Vec<Vec<&str>> {
+pub fn str_to_passphrases(i: &str) -> Vec<Vec<&str>> {
     i.lines().map(|l| str_to_passphrase(l)).collect()
 }
 
-fn to_hashset<'a, I>(i: I) -> HashSet<&'a str> where I: IntoIterator<Item = &'a str> {
+fn to_hashset<'a, I, S>(i: I) -> HashSet<S> where I: IntoIterator<Item = S>,
+S: Into<&'a str> + Eq + std::hash::Hash {
     i.into_iter().collect()
 }
 
-fn num_matching_lengths<'a, I>(i: I) -> usize where I: IntoIterator<Item = Vec<&'a str>> + Clone {
+fn to_hashset2<'a, I, S>(i: I) -> HashSet<S> where I: IntoIterator<Item = S>,
+S: Into<String> + Eq + std::hash::Hash {
+    i.into_iter().collect()
+}
+
+pub fn num_matching_lengths<'a, I>(i: I) -> usize where I: IntoIterator<Item = Vec<&'a str>> + Clone {
     let temp = i.clone().into_iter().map(to_hashset);
     i.into_iter().zip(temp)
         .map(|(a, b)| a.len() == b.len())
         .map(|b| if b {1} else {0})
         .sum()
+}
+
+fn sort_str<'a, 'b: 'a>(i: &'a str) -> String {
+    let mut chars: Vec<char> = i.chars().collect();
+    chars.sort();
+    let s = chars.into_iter().fold(String::new(), |mut acc, cur|
+        { acc.push(cur); acc });
+    s
+}
+
+pub fn part_two<'a, I>(i: I) -> usize where I: IntoIterator<Item = Vec<&'a str>> + Clone {
+    let mut count = 0;
+    for line in i {
+        let temp = line.clone();
+        let sorted_word_vec = temp.into_iter().map(sort_str);
+        let hashed = to_hashset2(sorted_word_vec);
+        if line.len() == hashed.len() { count += 1; }
+    }
+    count
 }

122d644de680a1162a3326da602ebcb192d2143a
Preston TW <preston_tunnell_wilson@brown.edu>
2018-10-16 20:50:32 -0400

    parse some lines

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -1,7 +1,8 @@
 // include nom
 #[macro_use]
 extern crate nom;
-use nom::{alpha, digit, line_ending, space};
+use nom::types::CompleteStr;
+use nom::{alpha, digit, space};
 
 #[derive(PartialEq, Debug)]
 struct Program {
@@ -10,42 +11,43 @@ struct Program {
   children: Option<Vec<String>>,
 }
 
-named!(weight<usize>, delimited!(char!('('), number, char!(')')));
+named!(weight<CompleteStr, usize>, delimited!(char!('('), number, char!(')')));
 
 #[test]
 fn test_weight() {
-  assert_eq!(weight(&b"(77)"[..]), Ok((&b""[..], 77_usize)));
+  assert_eq!(weight(CompleteStr("(77)")), Ok((CompleteStr(""), 77_usize)));
 }
 
+use std::str::FromStr;
+
 named!(
-  number<usize>,
-  map_res!(map_res!(digit, std::str::from_utf8), |s: &str| s
-    .parse::<usize>())
+  number<CompleteStr, usize>,
+  map_res!(recognize!(digit),
+  |cs: CompleteStr| usize::from_str(&cs))
 );
 
-named!(arrow, tag!("->"));
+named!(arrow<CompleteStr, CompleteStr>, tag!("->"));
 
 // use opt! for parsing children
 // opt!(arrow, space, many0(children), chars)
 
 named!(
-  children<Vec<String>>,
-  complete!(
+  children<CompleteStr, Vec<String>>,
   do_parse!(
     ws: separated_list!(tag!(", "), alpha)
       >> (ws
         .iter()
-        .map(|c| String::from_utf8(c.to_vec()).unwrap())
+        .map(|c| String::from_str(c).unwrap())
         .collect())
-  ))
+  )
 );
 
 #[test]
 fn test_children() {
   assert_eq!(
-    children(&b"abcd, defg, als"[..]),
+    children(CompleteStr("abcd, defg, als")),
     Ok((
-      &b""[..],
+      CompleteStr(""),
       vec!["abcd", "defg", "als"]
         .iter()
         .map(|c| c.to_string())
@@ -55,15 +57,14 @@ fn test_children() {
 }
 
 named!(
-  line<Program>,
+  line<CompleteStr, Program>,
   do_parse!(
     n: alpha
       >> space
       >> w: weight
       >> c: opt!(do_parse!(space >> arrow >> space >> c: children >> (c)))
-      >> line_ending
       >> (Program {
-        name: String::from_utf8(n.to_vec()).unwrap(),
+        name: String::from_str(&n).unwrap(),
         weight: w,
         children: c,
       })
@@ -73,9 +74,9 @@ named!(
 #[test]
 fn test_lines() {
   assert_eq!(
-    line(b"pbga (66)\n"),
+    line(CompleteStr("pbga (66)")),
     Ok((
-      &b""[..],
+      CompleteStr(""),
       Program {
         name: "pbga".to_string(),
         weight: 66,
@@ -85,9 +86,9 @@ fn test_lines() {
   );
 
   assert_eq!(
-    line(b"fwft (72) -> ktlj, cntj, xhth\n"),
+    line(CompleteStr("fwft (72) -> ktlj, cntj, xhth")),
     Ok((
-      &b""[..],
+      CompleteStr(""),
       Program {
         name: "fwft".to_string(),
         weight: 72,
@@ -103,19 +104,35 @@ fn test_lines() {
 }
 
 fn parse_lines(i: &str) -> Vec<Program> {
-  i.lines().map(|l| line(&l.as_bytes()[..]).unwrap().1).collect()
+  i.lines()
+    .map(|l| {
+      let res = line(CompleteStr(l));
+      res.unwrap().1
+    }).collect()
 }
 #[test]
 fn test_parse_lines() {
-  assert_eq!(parse_lines("ktlj (57)
-fwft (72) -> ktlj, cntj, xhth"),
-vec![Program {
-  name: "ktlj".to_string(),
-  weight: 57,
-  children: None,
-}, Program {
-  name: "fwft".to_string(),
-  weight: 72,
-  children: Some(vec!["ktlj", "cntj", "xhth"].iter().map(|c| c.to_string()).collect())
-}])
+  assert_eq!(
+    parse_lines(
+      "ktlj (57)
+fwft (72) -> ktlj, cntj, xhth"
+    ),
+    vec![
+      Program {
+        name: "ktlj".to_string(),
+        weight: 57,
+        children: None,
+      },
+      Program {
+        name: "fwft".to_string(),
+        weight: 72,
+        children: Some(
+          vec!["ktlj", "cntj", "xhth"]
+            .iter()
+            .map(|c| c.to_string())
+            .collect()
+        )
+      }
+    ]
+  )
 }

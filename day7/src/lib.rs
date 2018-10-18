12d9d03694f5d53a9ad46fc5ec5b6b53f37e9897
Preston TW <preston_tunnell_wilson@brown.edu>
2018-10-18 09:50:07 -0400

    get so close
    just get to compile then should work

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -3,7 +3,7 @@
 extern crate nom;
 use nom::types::CompleteStr;
 use nom::{alpha, digit, space};
-use std::collections::{HashSet, HashMap};
+use std::collections::{HashMap, HashSet};
 
 #[derive(PartialEq, Debug)]
 struct Program {
@@ -12,23 +12,80 @@ struct Program {
   children: Option<Vec<String>>,
 }
 
+fn build_platter<'a, I>(ps: I) -> HashMap<String, &'a Program>
+where
+  I: IntoIterator<Item = &'a Program>,
+{
+  ps.into_iter().fold(HashMap::new(), |mut acc, cur| {
+    acc.insert(cur.name.clone(), cur);
+    acc
+  })
+}
+
+fn total_weight_or_uneven_child(weights: &[usize]) -> Result<usize, usize> {
+  let weight_counts = weights
+    .iter()
+    .fold(HashMap::new(), |mut acc: HashMap<usize, usize>, cur| {
+      {
+        let count = acc.entry(*cur).or_insert(0);
+        *count += 1;
+      }
+      acc
+    });
+  // check if have multiple ones
+  let singulars: Vec<usize> = weight_counts
+    .iter()
+    .filter(|(_k, &v)| v == 1)
+    .map(|(k, _v)| *k)
+    .collect();
+
+  if singulars.len() == 1 {
+    Err(singulars[0])
+  } else if singulars.len() > 1 {
+    panic!(
+      "ambiguous weights, could change more than one! {:?}",
+      weights
+    );
+  }
+  // if don't, return sum of weights
+  else {
+    Ok(weights.iter().sum())
+  }
+}
+
 // return an either?
 pub fn balanced_weight(i: &str) -> usize {
   let root = bottom_program(i);
   let programs = parse_lines(i);
   // TODO build tree from programs
-  let tree: HashMap<String, Program> = HashMap::new();
+  let tree: HashMap<String, &Program> = build_platter(programs.iter());
 
-  fn children_weight(n: &str, tree: &HashMap<String, Program>) -> Result<usize, usize> {
+  fn children_weight(n: &str, tree: &HashMap<String, &Program>) -> Result<usize, usize> {
     let n = &tree[n];
     match n.children {
       None => Ok(n.weight),
       Some(ref children) => {
         // TODO check kids, if any are err, return that
         let ws = children.iter().map(|s: &String| children_weight(s, tree));
-        // if any is unequal, return that
-        // else, add current weight to total and return
-        Ok(n.weight)
+        let uneven: Vec<Result<usize, usize>> = ws.filter(|r: &Result<_, _>| r.is_err()).collect();
+        if uneven.len() == 1 {
+          uneven[0]
+        } else if uneven.len() >= 1 {
+          panic!(
+            "more than one child's platter is unbalanced! {:?}",
+            children
+          );
+        } else {
+          // if any weight is unequal of children, return that
+          let children_weight: Vec<usize> = ws.iter().map(|s| s.unwrap()).collect();
+          println!("{}: {:?}", n.name, children_weight);
+          let children_result = total_weight_or_uneven_child(&children_weight[..]);
+          // else, add current weight to total and return
+          match children_result {
+            Ok(children_sum) => Ok(children_sum + n.weight),
+            Err(u) => Err(u),
+          }
+        }
       }
     }
   }
@@ -57,7 +114,7 @@ gyxo (61)
 cntj (57)";
   assert_eq!(balanced_weight(input), 60_usize);
 
-  let simpleinput = "r (77) -> a b c
+  /* let simpleinput = "r (77) -> a b c
 a (6)
 b (6)
 c (7)";
@@ -72,7 +129,7 @@ o (1)
 c (1) -> h g
 h (3)
 g (3)";
-  assert_eq!(balanced_weight(diffinput), 4);
+  assert_eq!(balanced_weight(diffinput), 4);*/
 }
 
 pub fn bottom_program(i: &str) -> String {

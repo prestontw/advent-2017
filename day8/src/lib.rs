d3da5c9b69500762b992216845df2560b2f4a866
Preston TW <preston_tunnell_wilson@brown.edu>
2018-10-19 07:55:53 -0400

    add data types and include those data types

--- day8/src/lib.rs
+++ day8/src/lib.rs
@@ -2,3 +2,29 @@
 extern crate nom;
 use std::collections::HashMap;
 mod parse;
+
+enum Operation {
+  Increase,
+  Decrease,
+}
+enum Question {
+  gt,
+  lt,
+  eq,
+  ne,
+  ge,
+  le,
+}
+
+struct Condition {
+  register: String,
+  q: Question,
+  amount: isize, // this can't be another register, can it?
+}
+
+struct Instruction {
+  register: String,
+  op: Operation,
+  amount: isize,
+  cond: Condition,
+}
--- day8/src/parse.rs
+++ day8/src/parse.rs
@@ -1,3 +1,4 @@
+use super::{Instruction, Question, Condition, Operation::*};
 use nom::types::CompleteStr;
 use nom::{alpha, digit, space, be_i64, le_i64};
 use std::str::FromStr;
@@ -9,4 +10,19 @@ named!(parse_by<CompleteStr, isize>,
 fn test_numbers() {
   assert_eq!(parse_by(CompleteStr("-10")), Ok((CompleteStr(""), -10)));
   assert_eq!(parse_by(CompleteStr("10")), Ok((CompleteStr(""), 10)));
+}
+
+// todo: figure out how to include struct from lib
+#[test]
+fn test_line() {
+  let ideal = Instruction {
+    register: "a".to_string(),
+    op: Increase,
+    amount: 10,
+    cond: Condition {
+      register: "b".to_string(),
+      q: Question::le,
+      amount: 1
+    },
+  };
 }

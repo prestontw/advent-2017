427379a3c0276b639acd623f797c2a9b4e3b17e5
Preston TW <preston_tunnell_wilson@brown.edu>
2018-10-19 21:01:31 -0400

    add text to enum functions

--- day8/src/parse.rs
+++ day8/src/parse.rs
@@ -1,7 +1,14 @@
-use super::{Instruction, Question, Condition, Operation::*};
+use super::{Instruction, Question, Condition, Operation::{self, *}};
 use nom::types::CompleteStr;
 use nom::{alpha, digit, space, be_i64, le_i64};
 use std::str::FromStr;
+
+/* examples
+b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10
+*/
 
 named!(parse_by<CompleteStr, isize>,
   map_res!(recognize!(pair!(opt!(tag!("-")), digit)),
@@ -10,6 +17,26 @@ named!(parse_by<CompleteStr, isize>,
 fn test_numbers() {
   assert_eq!(parse_by(CompleteStr("-10")), Ok((CompleteStr(""), -10)));
   assert_eq!(parse_by(CompleteStr("10")), Ok((CompleteStr(""), 10)));
+}
+
+fn op_text_to_enum(s: &str) -> Operation {
+  match s {
+    "inc" => Operation::Increase,
+    "dec" => Operation::Decrease,
+    _ => panic!("unknown operation text: {}; options are `inc` or `dec", s),
+  }
+}
+
+fn question_text_to_enum(s: &str) -> Question {
+  match s {
+    ">" => Question::gt,
+    "<" => Question::lt,
+    ">=" => Question::ge,
+    "<=" => Question::le,
+    "==" => Question::eq,
+    "!=" => Question::ne,
+    _ => panic!("unknown question in condition: {}", s),
+  }
 }
 
 // todo: figure out how to include struct from lib

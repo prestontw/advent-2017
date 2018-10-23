96b40b1de75f8dcbf164401f28dc749d3b77da60
Preston TW <preston_tunnell_wilson@brown.edu>
2018-10-22 23:24:15 -0400

    add infra for main

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,3 +1,22 @@
+extern crate day9;
+use std::io::{self, Read};
+use nom::types::CompleteStr;
+
 fn main() {
-    println!("Hello, world!");
+     let input = {
+        let mut buffer = String::new();
+        let res = io::stdin().read_to_string(&mut buffer);
+        if let Err(e) = res {
+            panic!("could not read from stdin: {}", e)
+        }
+        buffer
+    };
+    let cleaned = day9::escape_bang_in_garbage(&input);
+    if let Ok((_remainder, group)) = day9::group(CompleteStr(&cleaned)) {
+      let score = day9::score_group(&group);
+    println!("{}", score);
+    }
+    else {
+        println!("could not parse input");
+    }
 }

e3464d6a2832247f217bedba6acc4db0b469c7fe
Preston Tunnell Wilson <preston_tunnell_wilson@brown.edu>
2018-10-25 14:39:34 -0400

    more work on part b
    small functions and make hashlist return a list

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -8,9 +8,33 @@ fn test_input_to_list() {
   assert_eq!(input_to_list("3,4"), vec![3, 4]);
 }
 
-pub fn hash_list(n: usize, lengths: &[usize]) -> usize {
+fn string_to_ascii_codes(i: &str) -> Vec<u8> {
+  i.as_bytes().to_vec()
+}
+#[test]
+fn test_part_two_input() {
+  assert_eq!(string_to_ascii_codes("1,2,3"), vec![49, 44, 50, 44, 51]);
+}
+
+fn append_usual(v: &mut Vec<u8>) {
+  let mut other = vec![17, 31, 73, 47, 23];
+  v.append(&mut other);
+}
+#[test]
+fn test_append() {
+  let mut base = vec![1, 2];
+  append_usual(&mut base);
+  assert_eq!(base, vec![1, 2, 17, 31, 73, 47, 23]);
+}
+
+pub fn get_hash_of_list(i: &[usize]) -> usize {
+  i[0] * i[1]
+}
+
+pub fn hash_list(n: usize, lengths: &[usize]) -> Vec<usize> {
   let mut start = initial_list(n);
 
+// move this out so can repeat hashlist thingy multiple times? or just loop lengths over and over?
   fn inner<'a>(lst: &'a mut[usize], cur_pos: usize, step_size: usize, length: usize) -> (&'a mut [usize], usize, usize) {
     let len = lst.len();
     let indices = wrap_indices(&start_plus_length_indices(cur_pos, length), len);
@@ -22,11 +46,11 @@ pub fn hash_list(n: usize, lengths: &[usize]) -> usize {
     inner(acc.0, acc.1, acc.2, *cur)
   });
 
-  start[0] * start[1]
+  start.to_vec()
 }
 #[test]
 fn test_example() {
-  assert_eq!(hash_list(5, &[3, 4, 1, 5]), 12);
+  assert_eq!(get_hash_of_list(&hash_list(5, &[3, 4, 1, 5])), 12);
 }
 /// Call this with n=256 to get [0, 255]
 pub fn initial_list(n: usize) -> Vec<usize> {
@@ -44,6 +68,32 @@ fn test_wrapping() {
   assert_eq!(wrap_indices(&vec![3, 4, 5, 6], 5), vec![3, 4, 0, 1]);
 }
 
+/// block of 16 numbers to one number
+fn xor_sparse_hash(i: &[u8]) -> u8 {
+  i.iter().fold(0, |acc, cur| acc ^ cur)
+}
+#[test]
+fn test_example_xor() {
+  assert_eq!(xor_sparse_hash(&vec![65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22]), 64);
+}
+
+fn number_to_hex(i: u8) -> String {
+  let result = format!("{:x}", i).to_string();
+  if result.len() == 1 {
+    // then prepend a 0
+    "0".to_owned() + &result
+  }
+  else {
+    result
+  }
+}
+#[test]
+fn test_numbers_to_hex() {
+    assert_eq!(number_to_hex(64), "40".to_string());
+    assert_eq!(number_to_hex(7), "07".to_string());
+    assert_eq!(number_to_hex(255), "ff".to_string());
+}
+
 fn reverse_values_at_indices<'a>(values: &'a mut [usize], indices: &[usize]) -> &'a mut [usize] {
   // could produce different first halves and second halves based on parity of length of list
   let (first_half, _second) = indices.split_at(indices.len() / 2);
@@ -58,7 +108,7 @@ fn reverse_values_at_indices<'a>(values: &'a mut [usize], indices: &[usize]) -> 
   values
 }
 #[test]
-fn testReverseValues() {
+fn test_reverse_values() {
   assert_eq!(reverse_values_at_indices(&mut vec![10, 11, 12, 13, 14], &vec![0, 1, 2]), &[12, 11, 10, 13, 14]);
   assert_eq!(reverse_values_at_indices(&mut vec![10, 11, 12, 13, 14], &vec![3, 4, 0, 1]), &[14, 13, 12, 11, 10]);
 }

d42a6a5547b28697757827793baec05cc9333738
Preston Tunnell Wilson <preston_tunnell_wilson@brown.edu>
2018-10-30 13:18:58 -0400

    add more tests

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -91,6 +91,18 @@ pub fn string_to_hash_string(i: &str) -> String {
 #[test]
 fn test_part_two() {
   assert_eq!(
+    string_to_hash_string("AoC 2017"),
+    "33efeb34ea91902bb2f59c9920caa6cd"
+  );
+  assert_eq!(
+    string_to_hash_string("1,2,3"),
+    "3efbe78a8d82f29979031a4aa0b16a9d"
+  );
+  assert_eq!(
+    string_to_hash_string("1,2,4"),
+    "63960835bcdc130f0b66d7ff4f6a5a8e"
+  );
+  assert_eq!(
     string_to_hash_string(""),
     "a2582a3a0e66e6e86e3812dcb672a272"
   );

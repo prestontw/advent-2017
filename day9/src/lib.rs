36870cfcc6c035b5796b5e8a4dadb3f80eafd8d4
Preston TW <preston_tunnell_wilson@brown.edu>
2018-11-06 20:05:27 -0500

    move parse file into lib
    seems better than asking people to look through files,
    my experience on github

--- day9/src/lib.rs
+++ day9/src/lib.rs
@@ -1,7 +1,185 @@
 #[macro_use]
 extern crate nom;
-pub mod parse;
 pub use self::parse::group;
+
+mod parse {
+  use super::Group;
+  use nom::types::CompleteStr;
+
+  named!(garbage<CompleteStr, Group>,
+  do_parse!(
+    body: delimited!(char!('<'), opt!(is_not!(">")), char!('>')) >>
+    (Group::Garbage(match body {
+      Some(cs) => cs.to_string(),
+      None => "".to_string()
+    }))));
+  #[test]
+  fn test_garbage() {
+    assert_eq!(
+      garbage(CompleteStr("<<<<>")),
+      Ok((CompleteStr(""), Group::Garbage("<<<".to_string())))
+    );
+  }
+
+  /**
+   * parsing a group can either be:
+   * empty group: {};
+   * garbage: garbage parser for this!;
+   * or recursive group: {group};
+   *
+   * Really, can combine the first and third as many0!(group).
+   * And, have to think of it many0!(alt!(group | garbage))
+   * since can alternate groups and garbage
+   * Need to include commas in here too...
+   */
+  named!(pub group<CompleteStr, Group>,
+  do_parse!(
+    tag!("{") >>
+    children:
+      separated_list!(tag!(","), alt!(group | garbage)) >>
+    tag!("}") >>
+    (Group::Group { children: children })
+  )
+);
+  #[test]
+  fn test_parse_groups() {
+    assert_eq!(
+      group(CompleteStr("{}")),
+      Ok((
+        CompleteStr(""),
+        Group::Group {
+          children: Vec::new()
+        }
+      ))
+    );
+    assert_eq!(
+      group(CompleteStr("{{{<,},<>}}}")),
+      Ok((
+        CompleteStr(""),
+        Group::Group {
+          children: vec![Group::Group {
+            children: vec![Group::Group {
+              children: vec![Group::Garbage("".to_string())]
+            }]
+          }]
+        }
+      ))
+    );
+    assert_eq!(
+      group(CompleteStr("{{}}")),
+      Ok((
+        CompleteStr(""),
+        Group::Group {
+          children: vec![Group::Group {
+            children: Vec::new()
+          }]
+        }
+      ))
+    );
+    assert_eq!(
+      group(CompleteStr("{{},{}}")),
+      Ok((
+        CompleteStr(""),
+        Group::Group {
+          children: vec![
+            Group::Group {
+              children: Vec::new()
+            },
+            Group::Group {
+              children: Vec::new()
+            }
+          ]
+        }
+      ))
+    );
+    assert_eq!(
+      group(CompleteStr("{{{},{},{{}}}}")),
+      Ok((
+        CompleteStr(""),
+        Group::Group {
+          children: vec![Group::Group {
+            children: vec![
+              Group::Group {
+                children: Vec::new()
+              },
+              Group::Group {
+                children: Vec::new()
+              },
+              Group::Group {
+                children: vec![Group::Group {
+                  children: Vec::new()
+                }]
+              }
+            ]
+          }]
+        }
+      ))
+    );
+    assert_eq!(
+      group(CompleteStr("{<a>,<a>,<a>,<a>}")),
+      Ok((
+        CompleteStr(""),
+        Group::Group {
+          children: vec![
+            Group::Garbage("".to_string()),
+            Group::Garbage("".to_string()),
+            Group::Garbage("".to_string()),
+            Group::Garbage("".to_string())
+          ]
+        }
+      ))
+    );
+    assert_eq!(
+      group(CompleteStr("{{<ab>},{<ab>},{<ab>},{<ab>}}")),
+      Ok((
+        CompleteStr(""),
+        Group::Group {
+          children: vec![
+            Group::Group {
+              children: vec![Group::Garbage("".to_string())]
+            },
+            Group::Group {
+              children: vec![Group::Garbage("".to_string())]
+            },
+            Group::Group {
+              children: vec![Group::Garbage("".to_string())]
+            },
+            Group::Group {
+              children: vec![Group::Garbage("".to_string())]
+            },
+          ]
+        }
+      ))
+    );
+    assert_eq!(
+      group(CompleteStr("{{<a},{<a},{<a},{<ab>}}")),
+      Ok((
+        CompleteStr(""),
+        Group::Group {
+          children: vec![Group::Group {
+            children: vec![Group::Garbage("".to_string())]
+          }]
+        }
+      ))
+    );
+    assert_eq!(
+      group(CompleteStr("{{{{<>}}}}")),
+      Ok((
+        CompleteStr(""),
+        Group::Group {
+          children: vec![Group::Group {
+            children: vec![Group::Group {
+              children: vec![Group::Group {
+                children: vec![Group::Garbage("".to_string())]
+              }]
+            }]
+          }]
+        }
+      ))
+    );
+  }
+
+}
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Group {
@@ -12,15 +190,20 @@ pub enum Group {
 pub fn count_chars_in_garbage(g: &Group) -> usize {
   match g {
     Group::Garbage(s) => s.len(),
-    Group::Group{children} => children.iter().map(count_chars_in_garbage).sum()
+    Group::Group { children } => children.iter().map(count_chars_in_garbage).sum(),
   }
 }
 #[test]
 fn test_counting() {
-  assert_eq!(count_chars_in_garbage(&Group::Group { children: vec![
-    Group::Garbage("123".to_string()),
-    Group::Garbage("12".to_string()),
-  ]}), 5);
+  assert_eq!(
+    count_chars_in_garbage(&Group::Group {
+      children: vec![
+        Group::Garbage("123".to_string()),
+        Group::Garbage("12".to_string()),
+      ]
+    }),
+    5
+  );
 }
 pub fn score_group(g: &Group) -> usize {
   fn score(g: &Group, acc: usize) -> usize {

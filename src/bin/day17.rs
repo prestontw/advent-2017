71cf1c47b95df7e2915e216e4520f5c112141f26
Preston TW <preston_tunnell_wilson@brown.edu>
2019-08-28 23:11:34 -0400

    day17 part1

---
+++ src/bin/day17.rs
@@ -0,0 +1,8 @@
+extern crate advent_2017;
+use advent_2017::day17;
+
+fn main() {
+    let input = 348;
+    let res1 = day17::part1(input);
+    println!("{}", res1);
+}
---
+++ src/day17.rs
@@ -0,0 +1,79 @@
+#[derive(Debug, PartialEq)]
+struct SpinLock {
+    position: usize,
+    buffer: Vec<usize>,
+}
+
+impl SpinLock {
+    fn new(capacity: usize) -> SpinLock {
+        let mut v = Vec::with_capacity(capacity);
+        v.push(0);
+        SpinLock {
+            position: 0,
+            buffer: v,
+        }
+    }
+    fn step(&mut self, num_steps: usize) {
+        let len = self.buffer.len();
+        self.position = (self.position + num_steps) % len;
+    }
+    fn insert(&mut self, value: usize) {
+        if self.position + 1 >= self.buffer.len() {
+            self.buffer.push(value);
+        } else {
+            self.buffer.insert(self.position + 1, value);
+        }
+        self.position += 1;
+    }
+    fn buffer(&self) -> Vec<usize> {
+        self.buffer.clone()
+    }
+}
+
+#[test]
+fn test_inserts() {
+    let mut spin = SpinLock::new(10);
+    let steps = 3;
+    spin.step(steps);
+    spin.insert(1);
+    assert_eq!(spin.buffer(), vec![0, 1]);
+    spin.step(steps);
+    spin.insert(2);
+    assert_eq!(spin.buffer(), vec![0, 2, 1]);
+    spin.step(steps);
+    spin.insert(3);
+    assert_eq!(spin.buffer(), vec![0, 2, 3, 1]);
+    spin.step(steps);
+    spin.insert(4);
+    assert_eq!(spin.buffer(), vec![0, 2, 4, 3, 1]);
+    spin.step(steps);
+    spin.insert(5);
+    assert_eq!(spin.buffer(), vec![0, 5, 2, 4, 3, 1]);
+    spin.step(steps);
+    spin.insert(6);
+    assert_eq!(spin.buffer(), vec![0, 5, 2, 4, 3, 6, 1]);
+    spin.step(steps);
+    spin.insert(7);
+    assert_eq!(spin.buffer(), vec![0, 5, 7, 2, 4, 3, 6, 1]);
+    spin.step(steps);
+    spin.insert(8);
+    assert_eq!(spin.buffer(), vec![0, 5, 7, 2, 4, 3, 8, 6, 1]);
+    spin.step(steps);
+    spin.insert(9);
+    assert_eq!(spin.buffer(), vec![0, 9, 5, 7, 2, 4, 3, 8, 6, 1]);
+}
+
+pub fn part1(steps: usize) -> usize {
+    let mut spin = SpinLock::new(2018);
+    for i in 1..=2017 {
+        spin.step(steps);
+        spin.insert(i);
+    }
+    let index = spin.buffer().into_iter().position(|c| c == 2017).unwrap();
+    spin.buffer()[(index + 1) % 2018]
+}
+
+#[test]
+fn test_part1() {
+    assert_eq!(part1(3), 638);
+}
